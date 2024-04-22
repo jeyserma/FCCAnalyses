6370bd95efc50a034cf8304e3008608c59d7da25
github-action-benchmark <github@users.noreply.github.com>
2024-04-22 07:57:50 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 534afb2207188e2b2785cdacf85aae6f784cd466

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713772669079,
+  "lastUpdate": 1713772670190,
   "repoUrl": "https://github.com/jeyserma/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -4331,6 +4331,55 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smieško",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "534afb2207188e2b2785cdacf85aae6f784cd466",
+          "message": "Temporarily adding back type for Reco particles (#366)",
+          "timestamp": "2024-04-19T15:50:13Z",
+          "tree_id": "efc364e3f62d556d83eda4a189e5c847f9a0e548",
+          "url": "https://github.com/jeyserma/FCCAnalyses/commit/534afb2207188e2b2785cdacf85aae6f784cd466"
+        },
+        "date": 1713772669852,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 9.374335292361845,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 9.57987919857625,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 7.25518048764287,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.849568389050312,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.220946794652624,
+            "unit": "Evt/s"
           }
         ]
       }
